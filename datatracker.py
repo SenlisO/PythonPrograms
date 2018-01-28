91266a3e0e4eb54b52124b1a6bfd0feb59911063
Senlis <gizmo.romick@gmail.com>
2018-01-27 22:40:23 -0900

    Finish datatracker program
    Minimum viable product is now complete and being tested.  Other parametrics may be added at a later date.

--- datatracker.py
+++ datatracker.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 from selenium import webdriver
+import time
+import smtplib  # email module
 
 # set up the firefox webdriver
 browser = webdriver.Firefox()
@@ -8,7 +11,7 @@ browser.get('https://login.gci.com/#quick-link')
 user_elem = browser.find_element_by_id('username')
 user_elem.send_keys("Senlis")
 pass_elem = browser.find_element_by_id('password')
-pass_elem.send_keys('REDACTED')
+pass_elem.send_keys('[REDACTED]')
 # pass_elem.submit()   this does not seem to work well
 login_button = browser.find_element_by_id('login')
 login_button.click()
@@ -17,5 +20,16 @@ login_button.click()
 browser.get('https://apps.gci.com/um/overview#quick-link')
 
 # print data usage
-#usage_elem = browser.find_element_by_class_name('total')
-#print(usage_elem.text)
+time.sleep(5)
+usage_elem = browser.find_element_by_class_name('total')
+print(usage_elem.text)
+days_elem = browser.find_element_by_class_name('days')
+print(days_elem.text)
+
+# send our message with all the data
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp_object:  # this opens an smtp object that exists within the indented block
+    smtp_object.starttls()
+    smtp_object.login('gizmocomputerservices@gmail.com', '[REDACTED]')
+    message_body = "Subject: Data Usage Report\n\n" + "Total data usage: " + usage_elem.text + "\nDays into billing cycle " + days_elem.text
+    print('message body: ' + message_body)
+    smtp_object.sendmail('gizmocomputerservices@gmail.com', 'gizmo.romick@gmail.com', message_body)
