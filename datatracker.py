ba498dfdb760cf35f0fa4c174b19b03d85981b6f
Senlis <gizmo.romick@gmail.com>
2018-03-25 14:58:09 -0800

    Add more GCI hardening, add login_to_ATT function, create create_browser_instance

--- datatracker.py
+++ datatracker.py
@@ -15,16 +15,43 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 
-def login_to_GCI(browser):
+def login_to_GCI(browser, wait):
     '''
     Function logs into GCI
     Parameters:
         browser: handle on Firefox browser instance
+        wait: object used to have browser handle wait on HTML elements to appear
     '''
-    # go to login screen, fill out username and password and then press login button
-    print("logging in to GCI")
+    page_loaded = False  # keeps us trying to log in until we find username/password field
+    count = 3  # eventually allows us to stop trying if we are still not successful
 
-    browser.get('https://login.gci.com/#quick-link')
+    while not page_loaded:
+        # navigate to GCI login page
+        print("logging in to GCI")
+        browser.get('https://login.gci.com/#quick-link')
+
+        try:
+            # wait until we see the username and password fields to appear
+            wait.until(expected.visibility_of_element_located((By.ID, 'username')))
+            wait.until(expected.visibility_of_element_located((By.ID, 'password')))
+            page_loaded = True  # if both previous elements are found, exit while loop
+        except TimeoutException:  # for some reason, we didn't find the username/password fields
+            print("GCI login timeout exception.  Count=" + str(count)) 
+            if count > 1:  # if we haven't tried more than twice
+                count -= 1  # just try again
+            elif count > 0:  # if we have tried twice, restart in headless mode
+                count -= 1
+
+                # restart browser instance in non-headless mode
+                headless_flag = False
+                browser, wait = create_browser_instance(headless_flag)
+            else:  # we have tried 3 times, pause for user to fix issue
+                # display error message and wait for user input
+                print("Timeout occurred without finding necessary data")
+                input("Please fix error and press enter to continue")
+                print("Re-navigating to login screen.  Apply necessary changes to script")
+
+    # enter username and password and then press login button
     user_elem = browser.find_element_by_id('username')
     user_elem.send_keys("Senlis")
     pass_elem = browser.find_element_by_id('password')
@@ -35,6 +62,54 @@ def login_to_GCI(browser):
     print("login complete")
 
 
+def login_to_ATT(browser, wait):
+    '''
+    Function navigates to AT&T login screen, enters username and password, and logs in
+    Parameters:
+        browser: handle on Firefox browser instance
+        wait: object used to have browser handle wait on HTML elements to appear
+    '''
+    page_loaded = False  # keeps us trying to log in until we find username/password field
+    count = 3  # eventually allows us to stop trying if we are still not successful
+    
+    # navigate to AT&T
+    print("logging into AT&T")
+    browser.get('https://www.att.com/my/#/login')
+
+    # todo: harden
+    # wait until we see the username and password fields to appear
+    wait.until(expected.visibility_of_element_located((By.ID, 'userName')))
+    wait.until(expected.visibility_of_element_located((By.ID, 'password')))
+
+    # enter username and password and then press login button
+    user_elem = browser.find_element_by_id('userName')
+    user_elem.send_keys("gizmo.romick@gmail.com")
+    pass_elem = browser.find_element_by_id('password')
+    pass_elem.send_keys("[REDACTED]")
+    login_button = browser.find_element_by_id('loginButton')
+    login_button.click()
+    # after last command, selenium navigates to "https://m.att.com/my/#/accountOverview"
+
+    print("login complete")
+
+
+def create_browser_instance(headless):
+    # set up the firefox webdriver
+    browser_options = Options()
+
+    # this if statement controls whether the browser starts headless (no visible browser window)
+    if (headless_flag):
+        browser_options.add_argument('-headless')
+
+        # create browser object
+        browser = webdriver.Firefox(firefox_options=browser_options)
+
+        # set default wait times
+        wait = WebDriverWait(browser, timeout=10)
+
+    return (browser, wait)
+
+
 # flags
 headless_flag = True  # headless means no visible browser window
 data_gathered = False  # when we get the data from the page we need, this flag allows us to continue
@@ -42,20 +117,9 @@ data_gathered = False  # when we get the data from the page we need, this flag a
 # variable
 count = 3  # used to monitor how many times we try to navigate to a page
 
-# set up the firefox webdriver
-browser_options = Options()
+browser, wait = create_browser_instance(headless_flag)  # create a browser
 
-# this if statement controls whether the browser starts headless (no visible browser window)
-if (headless_flag):
-    browser_options.add_argument('-headless')
-
-# create browser object
-browser = webdriver.Firefox(firefox_options=browser_options)
-
-# set default wait times
-wait = WebDriverWait(browser, timeout=10)
-
-login_to_GCI(browser)  # function will log in for us
+login_to_GCI(browser, wait)  # function will log in for us
 
 # navigate to data usage screen
 # tries twice in headless mode, and then restarts browser in normal mode
@@ -85,17 +149,11 @@ while not data_gathered:
         elif count > 1:  # we have already tried twice
             count -= 1  # decriment count once
             # restart browser in non-headless mode so we can see what we are doing
-            # set up the firefox webdriver
-            browser_options = Options()
-
-            # create browser object
-            browser = webdriver.Firefox(firefox_options=browser_options)
-
-            # set default wait times
-            wait = WebDriverWait(browser, timeout=10)
+            headless_flag = False
+            browser, wait = create_browser_instance(headless_flag)
 
             # we may need to log in again
-            login_to_GCI(browser)
+            login_to_GCI(browser, wait)
         else:
             # display error message
             print("Timeout occurred without finding necessary data")
@@ -117,22 +175,7 @@ GCI_percentage_into_period = int((GCI_days_into_period / 30) * 100)  # assumed a
 print("GCI data collected")
 print("-----------------------------------------------")
 
-# navigate to AT&T
-print("logging into AT&T")
-browser.get('https://www.att.com/my/#/login')
-
-# wait until we see the username and password fields to appear
-wait.until(expected.visibility_of_element_located((By.ID, 'userName')))
-wait.until(expected.visibility_of_element_located((By.ID, 'password')))
-
-# enter username and password and then press login button
-user_elem = browser.find_element_by_id('userName')
-user_elem.send_keys("gizmo.romick@gmail.com")
-pass_elem = browser.find_element_by_id('password')
-pass_elem.send_keys("[REDACTED]")
-login_button = browser.find_element_by_id('loginButton')
-login_button.click()
-# after last command, selenium navigates to "https://m.att.com/my/#/accountOverview"
+login_to_ATT(browser, wait)
 
 # prepare some variables for data scraping loop
 data_gathered = False
